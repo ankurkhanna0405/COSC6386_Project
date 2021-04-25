0ce1311b45f370c9ba6e5a5c629d6935bf83434f
adarshknaidu <54045248+adarshknaidu@users.noreply.github.com>
2021-04-25 13:59:58 -0400

    Add show CFG to fuzzer

--- SymbolicFuzzer.py
+++ SymbolicFuzzer.py
@@ -1,6 +1,12 @@
 from fuzzingbook.SymbolicFuzzer import  SimpleSymbolicFuzzer
 from inspect import getmembers, isfunction
 from examples.pyth_triplet import PythogareanTriplets
+from fuzzingbook.ControlFlow import PyCFG, CFGNode, to_graph, gen_cfg
+import inspect
+from graphviz import Source, Graph
+
+def show_cfg(fn, **kwargs):
+    return Source(to_graph(gen_cfg(inspect.getsource(fn)), **kwargs))
 
 #func = examples.pyth_triplet
 
@@ -10,3 +16,4 @@ paths = symfz_ct.get_all_paths(symfz_ct.fnenter)
 
 print(symfz_ct.extract_constraints(paths[0]))
 print(symfz_ct.solve_path_constraint(paths[0]))
+show_cfg(PythogareanTriplets)

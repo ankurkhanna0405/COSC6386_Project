5fd4fe4e94cc3953cf5358bb2826a9dab6ef215f
adarshknaidu <54045248+adarshknaidu@users.noreply.github.com>
2021-04-25 22:29:28 -0400

    Simple fuzzer code

--- SymbolicFuzzer.py
+++ SymbolicFuzzer.py
@@ -1,6 +1,7 @@
 from fuzzingbook.SymbolicFuzzer import  SimpleSymbolicFuzzer
 from inspect import getmembers, isfunction
 from examples.pyth_triplet import PythogareanTriplets
+from examples.check_triangle import check_triangle
 from fuzzingbook.ControlFlow import PyCFG, CFGNode, to_graph, gen_cfg
 import inspect
 from graphviz import Source, Graph
@@ -8,12 +9,24 @@ from graphviz import Source, Graph
 def show_cfg(fn, **kwargs):
     return Source(to_graph(gen_cfg(inspect.getsource(fn)), **kwargs))
 
-#func = examples.pyth_triplet
+# Simple symbolic fuzzer for example PythogareanTriplets.py
+print(" Fuzzer output for PythogareanTriplets")
 
-symfz_ct = SimpleSymbolicFuzzer(PythogareanTriplets)
-paths = symfz_ct.get_all_paths(symfz_ct.fnenter)
+simp_fzzr_pyth = SimpleSymbolicFuzzer(PythogareanTriplets)
+paths = simp_fzzr_pyth.get_all_paths(simp_fzzr_pyth.fnenter)
 
+for i in range(len(paths)):
+    print("Path",i)
+    print("Constraint found is: ",simp_fzzr_pyth.extract_constraints(paths[i]))
+    print("Z3 solver solution for the above constraint is:",simp_fzzr_pyth.solve_path_constraint(paths[i]))
+print("*************************----------------------------------*****************************************")
+# Simple symbolic fuzzer for example CheckTriangle.py
+print(" Fuzzer output for Check Triangle")
 
-print(symfz_ct.extract_constraints(paths[0]))
-print(symfz_ct.solve_path_constraint(paths[0]))
-show_cfg(PythogareanTriplets)
+simp_fzzr_triangle = SimpleSymbolicFuzzer(check_triangle)
+paths = simp_fzzr_triangle.get_all_paths(simp_fzzr_triangle.fnenter)
+
+for i in range(len(paths)):
+    print("Path",i)
+    print("Constraint found is: ",simp_fzzr_triangle.extract_constraints(paths[i]))
+    print("Z3 solver solution for the above constraint is:",simp_fzzr_triangle.solve_path_constraint(paths[i]))
--- examples/pyth_triplet.py
+++ examples/pyth_triplet.py
@@ -1,6 +1,5 @@
-
 def PythogareanTriplets(a:int ,b: int, c:int) -> str:
-    if (a*a + b*b ) == (c*c):
-        return "Pyth Triplet"
-    else:
-        return "Not Triplet"
+        if (a*a + b*b)  == (c*c):
+            return "Pyth Triplet"
+        else:
+            return " Not Triplet"
